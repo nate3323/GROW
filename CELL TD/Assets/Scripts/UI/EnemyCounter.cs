e8a194c59584c96096af3114ae6e93ec49f9c623
SgtKrieg <seanyancey14@yahoo.com>
2024-06-03 12:38:43 -0500

    Remove conflicting tags

--- CELL TD/Assets/Scripts/UI/EnemyCounter.cs
+++ CELL TD/Assets/Scripts/UI/EnemyCounter.cs
@@ -8,13 +8,6 @@ public class EnemyCounter : MonoBehaviour
     GameObject[] enemies;
     public Text enemyCountText;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
