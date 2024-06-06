61e8dd2ece12febccea5ebf8aa2a63625049177d
SgtKrieg <seanyancey14@yahoo.com>
2024-06-06 16:21:55 -0500

    Wave Counter Updated

--- CELL TD/Assets/Scripts/UI/WaveCount.cs
+++ CELL TD/Assets/Scripts/UI/WaveCount.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+using System;
+
 public class WaveCount : MonoBehaviour
 {
+    [SerializeField]
+    private WaveManager _waveCount;
+
     public TMP_Text WaveCountText;
 
+    private void Awake()
+    {
+        _waveCount = FindObjectOfType<WaveManager>();
+        _waveCount.WaveEnded += WaveNumber;
+    }
     void Update()
     {
+        
+    }
+
+    void WaveNumber(object Sender, EventArgs a)
+    {
         WaveCountText.text = "" + WaveManager.Instance.WaveNumber;
+    }
+
+    void OnDestroy()
+    {
+        _waveCount.WaveEnded -= WaveNumber;
     }
 }
